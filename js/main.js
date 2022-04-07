8afbfcdc0a38c930ea3a485c4fddb4ebf25efa46
Philipp Mendogralo <abkxj59@gmail.com>
2022-04-07 18:55:11 +0500

    Отображение и валидация формы

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-import './utils/show-photos.js';
+import {showPhotos} from './utils/show-photos.js';
+import {onUploadButtonClick} from './utils/upload-new-foto.js';
+import {validateUploadForm} from './utils/validate-upload-form.js';
+
+showPhotos();
+onUploadButtonClick();
+validateUploadForm();
--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,3 +1,18 @@
+const body = document.querySelector('body');
+
+const openModal = (modal) => {
+  modal.classList.remove('hidden');
+  body.classList.add('modal-open');
+};
+
+const closeModal = (modal) => {
+  modal.classList.add('hidden');
+  body.classList.remove('modal-open');
+};
+
+const isEscKey = (evt) => evt.key === 'Escape';
+const isOversideClick = (evt) => evt.target.classList.contains('overlay') || evt.target.classList.contains('big-picture__preview') || evt.target.classList.contains('img-upload__overlay');
+
 const getRandomInt = (a, b) => {
   if (a < 0 || b < 0) {
     return 'error! Number can\'t be negative';
@@ -12,4 +27,4 @@ const getRandomArrayElement = (array) => array[getRandomInt(0, array.length - 1)
 
 const isStringLengthAllow = (someString, maxLength) => someString.length <= maxLength;
 
-export {getRandomInt, getRandomArrayElement, isStringLengthAllow};
+export {openModal,closeModal, isEscKey, isOversideClick, getRandomInt, getRandomArrayElement, isStringLengthAllow};
